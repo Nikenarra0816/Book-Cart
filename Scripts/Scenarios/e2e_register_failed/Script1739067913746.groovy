d7c3ce83d7365fbab911a2c4b394d57637208709
Nikenarra0816 <Nikenarra0816@gmail.com>
2025-02-21 14:56:22 +0700

    Add Test Suite Collection

--- Scripts/Scenarios/e2e_register_failed/Script1739067913746.groovy
+++ Scripts/Scenarios/e2e_register_failed/Script1739067913746.groovy
@@ -17,7 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.callTestCase(findTestCase('Blocks/Reusable Test Case/Open_Browser'), [:], FailureHandling.STOP_ON_FAILURE)
+WebUI.callTestCase(findTestCase('Blocks/Reusable Test Case/Open_Browser'), [:], FailureHandling.STOP_ON_FAILURE) 
 
 WebUI.callTestCase(findTestCase('Blocks/Reusable Test Case/Navigate_URL_Register-Global'), [:], FailureHandling.STOP_ON_FAILURE)
 
